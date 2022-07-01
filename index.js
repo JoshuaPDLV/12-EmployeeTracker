ff73c01cee25d7feafc17cbe06287d6d43e52813
JoshuaPDLV <90582520+JoshuaPDLV@users.noreply.github.com>
2022-06-30 20:22:51 -0400

    github

---
+++ index.js
@@ -0,0 +1,52 @@
+
+const inquirer = require("inquirer");
+let Database = require("./async-db");
+let cTable = require("console.table");
+
+const db = new Database({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Shifting Shadows",
+    database: "cms"
+  });
+  
+async function getManagerNames() {
+    let query = "SELECT * FROM employee WHERE manager_id IS NULL";
+
+    const rows = await db.query(query);
+    //console.log("number of rows returned " + rows.length);
+    let employeeNames = [];
+    for(const employee of rows) {
+        employeeNames.push(employee.first_name + " " + employee.last_name);
+    }
+    return employeeNames;
+}
+
+async function getAddEmployeeInfo() {
+    const managers = await getManagerNames();
+    const roles = await getRoles();
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?"
+            },
+            {
+                type: "list",
+                message: "What is the employee's role?",
+                name: "role",
+                choices: [
+   
+                    ...roles
+                ]
+            },
+
+        ])
+}
